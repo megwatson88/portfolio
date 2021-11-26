7dee60650c1eee38358d765332d1b9df66de8372
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-11-25 21:37:29 -0600

    Updates

---
+++ precache-manifest.85c57f5ed2b4c3ef643ecf1c63468ed8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad51d633536a0e9e428e553252c55f89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896fc0bb84a6318893e9",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "df965181a9cde5487f3f",
+    "url": "/static/css/main.a644a37b.chunk.css"
+  },
+  {
+    "revision": "896fc0bb84a6318893e9",
+    "url": "/static/js/2.8df6efc9.chunk.js"
+  },
+  {
+    "revision": "df965181a9cde5487f3f",
+    "url": "/static/js/main.5ecec45a.chunk.js"
+  },
+  {
+    "revision": "015eb96dc6d20e45ac85",
+    "url": "/static/js/runtime~main.8d83e2bd.js"
+  },
+  {
+    "revision": "2523e37546ef898b8d3970877d3b8bc3",
+    "url": "/static/media/01-html-css-git-homework-demo.2523e375.png"
+  },
+  {
+    "revision": "7d6d45622337f6cc32e6c3d82e4c6883",
+    "url": "/static/media/ScreenShot.7d6d4562.png"
+  },
+  {
+    "revision": "891e73fbf74a1caa012fd160f005e1bd",
+    "url": "/static/media/Toilet SOS.891e73fb.jpg"
+  },
+  {
+    "revision": "4f583a35765c9778a0768a0af06b0b7c",
+    "url": "/static/media/Untitled copy.4f583a35.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
